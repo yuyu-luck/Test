1a0b8b3e5473822f865c34fc43396f3b718c3510
sun <https://github.com/yuyu-luck/Test.git>
2022-04-24 22:17:52 +0430

    网关模块

---
+++ springbootdemo-getway/src/main/java/com/example/springbootdemogetway/SpringbootdemoGetwayApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemogetway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemoGetwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdemoGetwayApplication.class, args);
+    }
+
+}
---
+++ springbootdemo-getway/src/test/java/com/example/springbootdemogetway/SpringbootdemoGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemogetway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdemoGetwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
