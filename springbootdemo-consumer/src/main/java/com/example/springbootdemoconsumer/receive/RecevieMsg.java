eeafc2e519643db6499a0b0ad8ab8563b63628fe
sun <https://github.com/yuyu-luck/Test.git>
2022-04-25 13:02:26 +0430

    修改文件

--- springbootdemo-consumer/src/main/java/com/example/springbootdemoconsumer/receive/RecevieMsg.java
+++ springbootdemo-consumer/src/main/java/com/example/springbootdemoconsumer/receive/RecevieMsg.java
@@ -1,19 +1,19 @@
-package com.example.springbootdemoconsumer.receive;
-
-import com.example.springbootdemoconsumer.fegin.UserFegin;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
-@RabbitListener(queues = "deadQueue")
-public class RecevieMsg {
-    @Autowired
-    private UserFegin userFegin;
-
-    @RabbitHandler
-    public void handleMsg(String tokenuuid){
-        userFegin.login(tokenuuid);
-    }
-}
+//package com.example.springbootdemoconsumer.receive;
+//
+//import com.example.springbootdemoconsumer.fegin.UserFegin;
+//import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+//import org.springframework.amqp.rabbit.annotation.RabbitListener;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@RabbitListener(queues = "deadQueue")
+//public class RecevieMsg {
+//    @Autowired
+//    private UserFegin userFegin;
+//
+//    @RabbitHandler
+//    public void handleMsg(String tokenuuid){
+//        userFegin.login(tokenuuid);
+//    }
+//}
