e7183168bf4c353bec261d0db5fa6bb102ddfe83
sun <https://github.com/yuyu-luck/Test.git>
2022-04-25 17:34:23 +0430

    修改代码

--- springbootdemo-provide/src/main/java/com/example/springbootdemoprovide/user/config/RestTemplateConfig.java
+++ springbootdemo-provide/src/main/java/com/example/springbootdemoprovide/user/config/RestTemplateConfig.java
@@ -1,24 +1,24 @@
-//package com.example.springbootdemoprovide.user.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.client.SimpleClientHttpRequestFactory;
-//import org.springframework.web.client.RestTemplate;
-//
-///**
-// * @author sha.li
-// * @date 2020-12-16
-//        */
-//@Configuration
-//public class RestTemplateConfig {
-//
-//    @Bean
-//    public RestTemplate restTemplate() {
-//        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-//        requestFactory.setConnectTimeout(5000);
-//        requestFactory.setReadTimeout(30000);
-//
-//        return new RestTemplate(requestFactory);
-//    }
-//
-//}
+package com.example.springbootdemoprovide.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author sha.li
+ * @date 2020-12-16
+        */
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(5000);
+        requestFactory.setReadTimeout(30000);
+
+        return new RestTemplate(requestFactory);
+    }
+
+}
