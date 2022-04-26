cf043a0d00da6f8000b87e9e9f2c411acb1cf9e1
sun <https://github.com/yuyu-luck/Test.git>
2022-04-26 18:39:11 +0430

    修改代码

--- springbootdemo-provide/src/main/java/com/example/springbootdemoprovide/SpringbootdemoProvideApplication.java
+++ springbootdemo-provide/src/main/java/com/example/springbootdemoprovide/SpringbootdemoProvideApplication.java
@@ -2,17 +2,21 @@ package com.example.springbootdemoprovide;
 
 import com.example.springbootdemoprovide.user.model.User;
 import com.example.springbootdemoprovide.user.service.UserService;
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
 @MapperScan(basePackages = {"com.example.springbootdemoprovide.user.mapper"})
 @EnableCaching
+@EnableSwagger2
+@EnableKnife4j
 public class SpringbootdemoProvideApplication {
 
     public static void main(String[] args) {
--- springbootdemo-provide/src/main/java/com/example/springbootdemoprovide/user/config/Swagger2Config.java
+++ springbootdemo-provide/src/main/java/com/example/springbootdemoprovide/user/config/Swagger2Config.java
@@ -1,9 +1,13 @@
 package com.example.springbootdemoprovide.user.config;
 
 
-import io.swagger.models.Contact;
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,30 +16,32 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * swagger 配置
+ */
 @Configuration
 @EnableSwagger2
+@EnableKnife4j
+@Import(BeanValidatorPluginsConfiguration.class)
+@ConditionalOnProperty(name = "enabled", prefix = "swagger", havingValue = "true", matchIfMissing = true)
 public class Swagger2Config {
-    @Bean
-    public Docket createRestApi() {
+
+    @Bean(value = "userApi")
+    @Order(value = 1)
+    public Docket groupRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                // 指定构建api文档的详细信息的方法：apiInfo()
-                .apiInfo(apiInfo())
+                .apiInfo(groupApiInfo())
                 .select()
-                // 指定要生成api接口的包路径，这里把controller作为包路径，生成controller中的所有接口
-                .apis(RequestHandlerSelectors.basePackage("ccom.example.springbootdemoprovide.user.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.example.springbootdemoprovide.user.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
 
-    private ApiInfo apiInfo() {
+    private ApiInfo groupApiInfo() {
         return new ApiInfoBuilder()
-                // 设置页面标题
-                .title("银企直联管理系统")
-                // 设置接口描述
-                .description("银企直联管理系统API")
-                // 设置联系方式
-                .contact(String.valueOf(new Contact()))
-                // 设置版本
+                .title("臻管家2.0-Swagger")
+                .description("<div style='font-size:16px;color:red;font-weight:bold'>臻管家2.0-Swagger</div>")
+                .termsOfServiceUrl("http:///localhost:7002/")
                 .version("1.0")
                 .build();
     }
