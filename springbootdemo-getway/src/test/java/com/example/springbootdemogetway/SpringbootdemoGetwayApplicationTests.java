a8722a674f76c3e32474789a59cdc710c2f22b49
sun <https://github.com/yuyu-luck/Test.git>
2022-06-01 12:12:14 +0430

    修改test

--- springbootdemo-getway/src/test/java/com/example/springbootdemogetway/SpringbootdemoGetwayApplicationTests.java
+++ springbootdemo-getway/src/test/java/com/example/springbootdemogetway/SpringbootdemoGetwayApplicationTests.java
@@ -1,12 +1,12 @@
 package com.example.springbootdemogetway;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootdemoGetwayApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
