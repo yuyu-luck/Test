365395746e038678765ba9bbeb41848bf94608a8
sun <https://github.com/yuyu-luck/Test.git>
2022-06-01 13:08:28 +0430

    修改网关启动类

--- springbootdemo-getway/src/main/java/com/example/springbootdemogetway/SpringbootdemoGetwayApplication.java
+++ springbootdemo-getway/src/main/java/com/example/springbootdemogetway/SpringbootdemoGetwayApplication.java
@@ -2,8 +2,10 @@ package com.example.springbootdemogetway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringbootdemoGetwayApplication {
 
     public static void main(String[] args) {
