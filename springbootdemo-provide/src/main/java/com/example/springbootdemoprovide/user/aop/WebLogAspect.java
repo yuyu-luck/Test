82c83cbacce59bdbe2be6f29e8f85810ac1610be
sun <https://github.com/yuyu-luck/Test.git>
2022-06-22 16:01:26 +0430

    修改

--- springbootdemo-provide/src/main/java/com/example/springbootdemoprovide/user/aop/WebLogAspect.java
+++ springbootdemo-provide/src/main/java/com/example/springbootdemoprovide/user/aop/WebLogAspect.java
@@ -14,34 +14,34 @@ import org.springframework.stereotype.Component;
 @Component
 @Order(-5)
 public class WebLogAspect {
-    private Logger logger= LoggerFactory.getLogger(this.getClass());
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     /**
-
      * 定义一个切入点.
-
+     * <p>
      * 解释下：
-
-     *
-
+     * <p>
+     * <p>
+     * <p>
      * ~ 第一个 * 代表任意修饰符及任意返回值.
-
+     * <p>
      * ~ 第二个 * 任意包名
-
+     * <p>
      * ~ 第三个 * 代表任意方法.
-
+     * <p>
      * ~ 第四个 * 定义在web包或者子包
-
+     * <p>
      * ~ 第五个 * 任意方法
-
+     * <p>
      * ~ .. 匹配任意数量的参数.
-
      */
 
     @Pointcut("execution(public * com.example.springbootdemoprovide.user.service.*.list(..))")
-    public void webLog(){}
+    public void webLog() {
+    }
 
     @AfterReturning("webLog()")
-    public void  doAfterReturning(JoinPoint joinPoint){
+    public void doAfterReturning(JoinPoint joinPoint) {
 
         // 处理完请求，返回内容
 
