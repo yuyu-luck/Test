fa70799e457cfce2a38c1d0a72a3376c1da8893f
tanxyc <tanxyc@yonyou.com>
2022-06-23 17:57:04 +0430

    修改

--- springbootdemo-consumer/src/main/java/com/example/springbootdemoconsumer/fegin/UserFegin.java
+++ springbootdemo-consumer/src/main/java/com/example/springbootdemoconsumer/fegin/UserFegin.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 @Component
-@FeignClient(name = "springbootdemo-provide", url = "http://192.168.1.4:7002/user")
+@FeignClient(name = "springbootdemo-provide", url = "http://192.168.184.1:7002/user")
 public interface UserFegin {
 
 //    @RequestMapping("/get")
