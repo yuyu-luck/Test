9af5fc0c877a89cd4fd41d17e4a96cd1700db217
tanxyc <tanxyc@yonyou.com>
2022-07-12 09:29:20 +0430

    修改

--- springbootdemo-provide/src/main/java/com/example/springbootdemoprovide/user/mapper/TOrderMapper.java
+++ springbootdemo-provide/src/main/java/com/example/springbootdemoprovide/user/mapper/TOrderMapper.java
@@ -1,12 +1,10 @@
 package com.example.springbootdemoprovide.user.mapper;
 
 
-
-
 /**
-* @Entity com.example.springbootdemoprovide.user.model.TOrder
-*/
-public interface TOrderMapper  {
+ * @Entity com.example.springbootdemoprovide.user.model.TOrder
+ */
+public interface TOrderMapper {
 
 
 }
