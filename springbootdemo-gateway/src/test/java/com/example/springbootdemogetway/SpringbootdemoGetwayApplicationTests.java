80bc2a10ebc3088082ac52e8a3bf93e0d9f66a04
tanxyc <tanxyc@yonyou.com>
2022-07-27 15:44:30 +0430

    修改文件

--- springbootdemo-gateway/src/test/java/com/example/springbootdemogetway/SpringbootdemoGetwayApplicationTests.java
+++ springbootdemo-gateway/src/test/java/com/example/springbootdemogetway/SpringbootdemoGetwayApplicationTests.java
@@ -1,12 +1,13 @@
 package com.example.springbootdemogetway;
 
 //import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootdemoGetwayApplicationTests {
 
-//    @Test
+    //    @Test
     void contextLoads() {
     }
 
