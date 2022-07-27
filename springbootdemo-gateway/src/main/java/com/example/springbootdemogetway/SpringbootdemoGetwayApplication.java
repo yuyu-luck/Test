622db071ed4ee7d51797da8230c1d6677ed1df96
tanxyc <tanxyc@yonyou.com>
2022-07-27 16:46:57 +0430

    修改文件

--- springbootdemo-gateway/src/main/java/com/example/springbootdemogetway/SpringbootdemoGetwayApplication.java
+++ springbootdemo-gateway/src/main/java/com/example/springbootdemogetway/SpringbootdemoGetwayApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringbootdemoGetwayApplication {
 
     public static void main(String[] args) {
