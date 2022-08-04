fabf4dc728427ca5ee6ffde771c9ea0e8bb0ef2c
tanxyc <tanxyc@yonyou.com>
2022-08-04 09:35:09 +0430

    修改

--- springbootdemo-provide/src/main/java/com/example/springbootdemoprovide/user/controller/UserController.java
+++ springbootdemo-provide/src/main/java/com/example/springbootdemoprovide/user/controller/UserController.java
@@ -167,7 +167,7 @@ public class UserController {
         return mode;
     }
 
-    @RequestMapping
+    @RequestMapping("/index")
     @ResponseBody
     public ModelAndView index() {
         //request.setAttribute("name","nice to meet you");
