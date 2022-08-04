db5db61a592aa18e8e15a77927b7a1ea1deaf528
tanxyc <tanxyc@yonyou.com>
2022-08-04 17:20:26 +0430

    修改

--- springbootdemo-provide/src/main/java/com/example/springbootdemoprovide/user/scheduling/DynamicScheduleTask.java
+++ springbootdemo-provide/src/main/java/com/example/springbootdemoprovide/user/scheduling/DynamicScheduleTask.java
@@ -1,45 +1,45 @@
-package com.example.springbootdemoprovide.user.scheduling;
-
-import com.example.springbootdemoprovide.user.mapper.TokenMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.SchedulingConfigurer;
-import org.springframework.scheduling.config.ScheduledTaskRegistrar;
-import org.springframework.scheduling.support.CronTrigger;
-import org.springframework.util.StringUtils;
-
-import java.time.LocalDateTime;
-
-@Configuration      //1.主要用于标记配置类，兼备Component的效果。
-@EnableScheduling   // 2.开启定时任务
-public class DynamicScheduleTask implements SchedulingConfigurer {
-
-
-    @Autowired
-    private TokenMapper tokenMapper;
-
-    /**
-     * 执行定时任务.
-     */
-    @Override
-    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
-
-        taskRegistrar.addTriggerTask(
-                //1.添加任务内容(Runnable)
-                () -> System.out.println("执行动态定时任务: " + LocalDateTime.now().toLocalTime()),
-                //2.设置执行周期(Trigger)
-                triggerContext -> {
-                    //2.1 从数据库获取执行周期
-                    String cron = tokenMapper.getInfo();
-                    //2.2 合法性校验.
-                    if (StringUtils.isEmpty(cron)) {
-                        // Omitted Code ..
-                    }
-                    //2.3 返回执行周期(Date)
-                    return new CronTrigger(cron).nextExecutionTime(triggerContext);
-                }
-        );
-    }
-
-}
+//package com.example.springbootdemoprovide.user.scheduling;
+//
+//import com.example.springbootdemoprovide.user.mapper.TokenMapper;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.scheduling.annotation.SchedulingConfigurer;
+//import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+//import org.springframework.scheduling.support.CronTrigger;
+//import org.springframework.util.StringUtils;
+//
+//import java.time.LocalDateTime;
+//
+//@Configuration      //1.主要用于标记配置类，兼备Component的效果。
+//@EnableScheduling   // 2.开启定时任务
+//public class DynamicScheduleTask implements SchedulingConfigurer {
+//
+//
+//    @Autowired
+//    private TokenMapper tokenMapper;
+//
+//    /**
+//     * 执行定时任务.
+//     */
+//    @Override
+//    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+//
+//        taskRegistrar.addTriggerTask(
+//                //1.添加任务内容(Runnable)
+//                () -> System.out.println("执行动态定时任务: " + LocalDateTime.now().toLocalTime()),
+//                //2.设置执行周期(Trigger)
+//                triggerContext -> {
+//                    //2.1 从数据库获取执行周期
+//                    String cron = tokenMapper.getInfo();
+//                    //2.2 合法性校验.
+//                    if (StringUtils.isEmpty(cron)) {
+//                        // Omitted Code ..
+//                    }
+//                    //2.3 返回执行周期(Date)
+//                    return new CronTrigger(cron).nextExecutionTime(triggerContext);
+//                }
+//        );
+//    }
+//
+//}
--- springbootdemo-provide/src/main/java/com/example/springbootdemoprovide/user/scheduling/SpringTaskJob.java
+++ springbootdemo-provide/src/main/java/com/example/springbootdemoprovide/user/scheduling/SpringTaskJob.java
@@ -1,18 +1,18 @@
-package com.example.springbootdemoprovide.user.scheduling;
-
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-/**
- * spring提供的SpringTask定时任务
- * 我们只要在你要执行的任务上使用@Scheduled代表这是一个定时任务
- */
-@Component
-public class SpringTaskJob {
-
-    @Scheduled(cron = "0/1 * * * * ?")
-    public void task() {
-        System.out.println(Thread.currentThread().getName() + "韩波hhh");
-    }
-}
-
+//package com.example.springbootdemoprovide.user.scheduling;
+//
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * spring提供的SpringTask定时任务
+// * 我们只要在你要执行的任务上使用@Scheduled代表这是一个定时任务
+// */
+//@Component
+//public class SpringTaskJob {
+//
+//    @Scheduled(cron = "0/1 * * * * ?")
+//    public void task() {
+//        System.out.println(Thread.currentThread().getName() + "韩波hhh");
+//    }
+//}
+//
